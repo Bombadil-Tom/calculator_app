c37938693475dbc2fbf83a8ae477f16009c80177
Bombadil-Tom <uxhuber@gmail.com>
2019-08-13 21:40:20 -0700

    changing a variable declaration in dbConnect and cleanung up index.js

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -5,7 +5,7 @@ const keys = require('./config/keys');
 const url =`mongodb+srv://${keys.dbUser}:${keys.dbPassword}@cluster0-qyc2d.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(url, {useNewUrlParser: true});
-let db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('worked');
--- server/index.js
+++ server/index.js
@@ -4,15 +4,12 @@ const express = require('express');
 require('./models/CalculationSchema');
 require('./dbConnect');
 
-
 const port = 3000;
 const app = express();
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 require('./routes/calculatorApi')(app);
-
 
 app.listen(port, () => console.log(`app listening to port ${port}`));
